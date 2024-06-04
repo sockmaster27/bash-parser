92dd8e47881e9a4dc2654518c97627c892e463b6
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-06-05 00:24:37 +0200

    Inline `filter-iterator` and `reverse-arguments`

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,8 +1,12 @@
 'use strict';
-const filterIterator = require('filter-iterator');
-const reverse = require('reverse-arguments');
 const curry = require('lodash.curry');
 
-const filter = curry(reverse(filterIterator), 2);
+function* filterIterator(pred, xs) {
+	for (const x of xs) {
+		if (pred(x)) yield x;
+	}
+}
+
+const filter = curry(filterIterator, 2);
 
 module.exports = filter;
