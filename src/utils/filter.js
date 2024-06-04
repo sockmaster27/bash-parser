db6e2cbb75528b2e23e6a1f2fb603adbb37c89ed
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-06-05 00:26:43 +0200

    simplify

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,12 +1,10 @@
 'use strict';
 const curry = require('lodash.curry');
 
-function* filterIterator(pred, xs) {
+function* filter(pred, xs) {
 	for (const x of xs) {
 		if (pred(x)) yield x;
 	}
 }
 
-const filter = curry(filterIterator, 2);
-
-module.exports = filter;
+module.exports = curry(filter, 2);
